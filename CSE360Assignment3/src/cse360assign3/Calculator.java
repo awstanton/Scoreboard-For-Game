835d3f9025697a7951cb85d18d42fb0d0068ca2f
Andrew Stanton <dstanto2@asu.edu>
2016-02-21 20:36:14 -0700

    First commit of Assignment 3 for CSE 360.

---
+++ CSE360Assignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,68 @@
+// following source was used for reminder on how to generate JUNIT test cases:
+// https://courses.cs.washington.edu/courses/cse143/11wi/eclipse-tutorial/junit.shtml
+
+package cse360assign3;
+/**
+ * 
+ * @author Andrew Stanton, PIN: 817
+ *
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Calculator constructor that initializes total
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * returns the Calculator's total value
+	 * @returns the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds input to the total
+	 * @param value the value to be added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtracts input from the total
+	 * @param value the value to be subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiplies the total by the input
+	 * @param value the value total is to be multiplied by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divides the total by the input
+	 * @param value the value total is to be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * returns a string of the history of operations performed on Calculator's total
+	 * @return string of history of calculator operations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
